a25c8d719c8821ebf74726d2a9651ef24839a632
dany-datcom <djimenezgonzalez@byupathway.com>
2026-03-12 07:38:50 -0600

    arrow function in categories.js was added

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,10 +1,10 @@
 import db from './db.js';
 
-export async function getAllCategories() {
+export const getAllCategories = async () => {
     const result = await db.query(`
         SELECT category_id, name
         FROM categories
         ORDER BY name
     `);
     return result.rows;
-}
+};
